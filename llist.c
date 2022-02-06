40baf4d33cd33bcc38a559995b03ca052d46f377
Melissa Swearingen <swearingenmj@gmail.com>
2022-02-05 20:50:03 -0800

    edited errors in print function and node malloc function

--- llist.c
+++ llist.c
@@ -1,55 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/mman.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <sys/mman.h>
+#include "llist.h"
 
-// create a node class
-// struct Node {
-//     void *data;
-//     struct Node *next;
-// };
+// void llist_insert_head(struct node **head, struct node *n) {
+//     // TODO: code to insert at head
+// }
 
-struct Node {
-    int value;
-    struct Node *next;
-};
+// void node *llist_delete_head(struct node **head) {
 
-int node_alloc(int value) {
-    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+// }
+
+// void llist_insert_tail(struct node **head, struct node *n) {
+
+// }
+
+void llist_print(struct node *head) {
+    struct node* p = head;
+    while (p) {
+        printf("%d -> ", p->value);
+        p = p->next;
+    }
+    printf("NULL");
+}
+
+// void llist_free(struct node **head) {
+
+// }
+
+struct node *node_alloc(int value) {
+    struct node* node = (struct node*) malloc(sizeof(struct node));
     node->value = value;
     node->next = NULL;
     return node;
 }
 
-void llist_insert_head(struct node **head, struct node *n) {
-    // TODO: code to insert at head
-}
+// void node_free(struct node *n) {
 
-void llist_delete_head(struct node **head) {
-
-}
-
-void llist_insert_tail(struct node **head, struct node *n) {
-
-}
-
-void llist_print(struct node *head) {
-    while (node != NULL) {
-        printf("%d ", node->value);
-        node =node->next;
-    }
-    // struct Node* p = head;
-    // while (p) {
-    //     printf("%d -> ", p->value);
-    //     p = p->next;
-    // }
-    // printf("NULL");
-}
-
-void llist_free(struct node *n) {
-
-}
+// }
 
 int main(int argc, char *argv[]) {
 
@@ -57,11 +47,13 @@ int main(int argc, char *argv[]) {
 
     struct node *n = node_alloc(10);
     llist_insert_head(&head, n);
+    
+    return 0;
 }
 
 # if 0
 write command line parser
-    make sure you're getting the right numbers with the right
+    make sure youre getting the right numbers with the right
     commands. 
     either parse the whole command into some structure first, 
     or you can just run list commands as you process argv
